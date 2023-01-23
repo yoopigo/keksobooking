8910ac54c2d7401a0f2d38b35c7f6f4781dfba9d
yoopigo <collerggggggg@gmail.com>
2023-01-23 20:10:01 +0500

    js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 const COUNT = 10;
+const TYPE = ["palace", "flat", "house", "bungalow"];
+const CHECKIN = ["12:00", "13:00", "14:00"];
+const DESCRIPTION = [
+  "Номер высшей категории с большой кроватью",
+  "Представительский люкс",
+  "Стандартный номер с большой кроватью",
+  "Стандартный номер с двуспальной кроватью",
+  "Семейный номер с большой кроватью",
+  "Стандарный номер с двумя односпальными кроватями",
+  "Улучшенный номер с большой кровью",
+  "Номер для людей с ограниченными возможностями",
+  "Экономный номер с двумя кроватями",
+  "Экономный номер с одной кроватью",
+];
 
 const getRandomArrayElement = (elements) => {
   return elements[_.random(0, elements.length - 1)];
@@ -16,6 +30,21 @@ let createProfile = () => {
 
     let addProfile = {
       author: { avatar: avatarNumber + [i] },
+      offer: {
+        title: "Лучший номер " + [i],
+        address: _.random(1.1, 9.9) + ", " + _.random(1.2, 9.9),
+        price: _.random(1000, 10000),
+        type: getRandomArrayElement(TYPE),
+        rooms: _.random(1, 5),
+        guests: _.random(1, 5),
+        checkin: getRandomArrayElement(CHECKIN),
+        checkout: getRandomArrayElement(CHECKIN),
+        description: getRandomArrayElement(DESCRIPTION),
+      },
+      location: {
+        x: _.random(35.65, 35.7),
+        y: _.random(139.7, 139.8),
+      },
     };
     return addProfile;
   }
