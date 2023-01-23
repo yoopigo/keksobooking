550739178c1ba1bf3f24fac3f34bdf46e094d105
yoopigo <collerggggggg@gmail.com>
2023-01-23 18:43:29 +0500

    js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-const AVATAR_NUMBER = [01, 02, 03, 04, 05, 06, 07, 08, 09, 10];
+const COUNT = 10;
 
 const getRandomArrayElement = (elements) => {
   return elements[_.random(0, elements.length - 1)];
 };
 
-const createProfile = () => {
-  let addProfile = {
-    author: { avatar: "img/avatars/user" },
-  };
-  return addProfile;
+let createProfile = () => {
+  for (let i = 1; i <= COUNT; i++) {
+    let avatarNumber;
+
+    if (i < 10) {
+      avatarNumber = "img/avatars/user0";
+    } else {
+      avatarNumber = "img/avatars/user";
+    }
+
+    let addProfile = {
+      author: { avatar: avatarNumber + [i] },
+    };
+    return addProfile;
+  }
 };
 
 console.log(createProfile());
