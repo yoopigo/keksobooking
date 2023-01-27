7819e9623cd6b4ab374b5f845831f985167b889a
yoopigo <collerggggggg@gmail.com>
2023-01-27 22:24:09 +0500

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-undef */
+/* eslint-disable no-console */
 'use strict';
 
-const similarProfile1 = [];
-
 const COUNT = 10;
+
 const TYPE = ['palace', 'flat', 'house', 'bungalow'];
+
 const CHECKIN = ['12:00', '13:00', '14:00'];
+
 const DESCRIPTION = [
   'Номер высшей категории с большой кроватью',
   'Представительский люкс',
@@ -19,43 +21,78 @@ const DESCRIPTION = [
   'Экономный номер с одной кроватью',
 ];
 
+let features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
+
+const ROOMS = {
+  MIN: 1,
+  MAX: 5,
+};
+
+const GUESTS = {
+  MIN: 1,
+  MAX: 5,
+};
+
+const PRICE = {
+  MIN: 1000,
+  MAX: 10000,
+};
+
+const LOCATIONX = {
+  MIN: 35.65,
+  MAX: 35.7,
+};
+
+const LOCATIONY = {
+  MIN: 139.7,
+  MAX: 139.8,
+};
+
+const FEATURESCOUNT = {
+  MIN: 1,
+  MAX: 5,
+};
+
+const similarProfile = [];
+
+//  Взятие рамдомного элемента массива
 const getRandomArrayElement = (elements) => {
   return elements[_.random(0, elements.length - 1)];
 };
 
-let createProfile = () => {
+// Функция создания рандомного профиля
+const createProfile = () => {
   for (let i = 1; i <= COUNT; i++) {
-    let avatarNumber;
-
-    if (i < 10) {
-      avatarNumber = 'img/avatars/user0';
-    } else {
-      avatarNumber = 'img/avatars/user';
+    let avatarCount = 'img/avatars/user0';
+    if (i >= 10) {
+      avatarCount = 'img/avatars/user';
     }
 
-    let addProfile = {
-      author: { avatar: avatarNumber + i },
+    similarProfile.push({
+      author: { avatar: avatarCount + i },
       offer: {
         title: 'Лучший номер ' + i,
         address: _.random(1.1, 9.9) + ', ' + _.random(1.2, 9.9),
-        price: _.random(1000, 10000),
+        price: _.random(PRICE.MIN, PRICE.MAX),
         type: getRandomArrayElement(TYPE),
-        rooms: _.random(1, 5),
-        guests: _.random(1, 5),
+        rooms: _.random(ROOMS.MIN, ROOMS.MAX),
+        guests: _.random(GUESTS.MIN, GUESTS.MAX),
         checkin: getRandomArrayElement(CHECKIN),
         checkout: getRandomArrayElement(CHECKIN),
         description: getRandomArrayElement(DESCRIPTION),
+        features: [
+          getRandomArrayElement(features),
+          getRandomArrayElement(features),
+        ],
       },
       location: {
-        x: _.random(35.65, 35.7),
-        y: _.random(139.7, 139.8),
+        x: _.random(LOCATIONX.MIN, LOCATIONX.MAX),
+        y: _.random(LOCATIONY.MIN, LOCATIONY.MAX),
       },
-    };
-    return addProfile;
+    });
   }
 };
 
-const similarProfile = new Array(COUNT).fill(null).map(() => createProfile());
+createProfile();
 
 console.log(similarProfile);
-console.log(similarProfile1);
