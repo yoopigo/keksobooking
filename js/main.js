afa27a905ff2ed48a25cfb603996069501dd26e3
yoopigo <collerggggggg@gmail.com>
2023-01-27 19:00:41 +0500

    js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const similarProfile1 = [];
+
 const COUNT = 10;
-const TYPE = ["palace", "flat", "house", "bungalow"];
-const CHECKIN = ["12:00", "13:00", "14:00"];
+const TYPE = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKIN = ['12:00', '13:00', '14:00'];
 const DESCRIPTION = [
-  "Номер высшей категории с большой кроватью",
-  "Представительский люкс",
-  "Стандартный номер с большой кроватью",
-  "Стандартный номер с двуспальной кроватью",
-  "Семейный номер с большой кроватью",
-  "Стандарный номер с двумя односпальными кроватями",
-  "Улучшенный номер с большой кровью",
-  "Номер для людей с ограниченными возможностями",
-  "Экономный номер с двумя кроватями",
-  "Экономный номер с одной кроватью",
+  'Номер высшей категории с большой кроватью',
+  'Представительский люкс',
+  'Стандартный номер с большой кроватью',
+  'Стандартный номер с двуспальной кроватью',
+  'Семейный номер с большой кроватью',
+  'Стандарный номер с двумя односпальными кроватями',
+  'Улучшенный номер с большой кровью',
+  'Номер для людей с ограниченными возможностями',
+  'Экономный номер с двумя кроватями',
+  'Экономный номер с одной кроватью',
 ];
 
 const getRandomArrayElement = (elements) => {
@@ -23,16 +28,16 @@ let createProfile = () => {
     let avatarNumber;
 
     if (i < 10) {
-      avatarNumber = "img/avatars/user0";
+      avatarNumber = 'img/avatars/user0';
     } else {
-      avatarNumber = "img/avatars/user";
+      avatarNumber = 'img/avatars/user';
     }
 
     let addProfile = {
-      author: { avatar: avatarNumber + [i] },
+      author: { avatar: avatarNumber + i },
       offer: {
-        title: "Лучший номер " + [i],
-        address: _.random(1.1, 9.9) + ", " + _.random(1.2, 9.9),
+        title: 'Лучший номер ' + i,
+        address: _.random(1.1, 9.9) + ', ' + _.random(1.2, 9.9),
         price: _.random(1000, 10000),
         type: getRandomArrayElement(TYPE),
         rooms: _.random(1, 5),
@@ -50,4 +55,7 @@ let createProfile = () => {
   }
 };
 
-console.log(createProfile());
+const similarProfile = new Array(COUNT).fill(null).map(() => createProfile());
+
+console.log(similarProfile);
+console.log(similarProfile1);
