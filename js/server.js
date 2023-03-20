913429d0a6b295172f1e5c470b21dc29a3143db6
yoopigo <collerggggggg@gmail.com>
2023-03-20 21:05:04 +0500

    js

--- js/server.js
+++ js/server.js
@@ -1,9 +1,30 @@
 import { generation } from './map.js';
 
 const adCounter = 100;
+const ALLERT_SHOW_TIME = 3000;
 
 fetch('https://23.javascript.pages.academy/keksobooking/data')
-  .then((response) => response.json())
-  .then((data) => {
-    generation(data.slice(0, adCounter));
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json().then((data) => generation(data.slice(0, adCounter)));
+  })
+  .catch((err) => {
+    const alertContainer = document.createElement('div');
+    alertContainer.style.zIndex = '9999';
+    alertContainer.style.position = 'absolute';
+    alertContainer.style.left = '0';
+    alertContainer.style.right = '0';
+    alertContainer.style.top = '0';
+    alertContainer.style.fontSize = '20px';
+    alertContainer.style.color = 'red';
+    alertContainer.style.textAlign = 'center';
+    alertContainer.style.backgroundColor = 'white';
+    alertContainer.textContent = `Ошибка соединения с сервером: ${err}`;
+    document.body.appendChild(alertContainer);
+
+    setTimeout(() => {
+      alertContainer.remove();
+    }, ALLERT_SHOW_TIME);
   });
