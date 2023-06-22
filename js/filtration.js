4fd18f30198690dc09279e3fbd673f0954098408
yoopigo <collerggggggg@gmail.com>
2023-06-22 21:57:49 +0500

    фильтрация

---
+++ js/filtration.js
@@ -0,0 +1,62 @@
+const filterData = (
+  data,
+  selectedType,
+  selectedPrice,
+  selectedRoom,
+  selectedGuests
+) => {
+  let filteredData = data;
+  if (selectedType !== 'any') {
+    filteredData = filteredData.filter(
+      (item) => item.offer.type === selectedType
+    );
+  }
+
+  if (selectedPrice !== 'any') {
+    switch (selectedPrice) {
+      case 'low':
+        filteredData = filteredData.filter((item) => item.offer.price < 10000);
+        break;
+      case 'middle':
+        filteredData = filteredData.filter(
+          (item) => item.offer.price >= 10000 && item.offer.price <= 50000
+        );
+        break;
+      case 'high':
+        filteredData = filteredData.filter((item) => item.offer.price > 50000);
+        break;
+    }
+  }
+
+  if (selectedRoom !== 'any') {
+    switch (selectedRoom) {
+      case '1':
+        filteredData = filteredData.filter((item) => item.offer.rooms == 1);
+        break;
+      case '2':
+        filteredData = filteredData.filter((item) => item.offer.rooms == 2);
+        break;
+      case '3':
+        filteredData = filteredData.filter((item) => item.offer.rooms == 3);
+        break;
+    }
+  }
+
+  if (selectedGuests !== 'any') {
+    switch (selectedGuests) {
+      case '0':
+        filteredData = filteredData.filter((item) => item.offer.guests == 0);
+        break;
+      case '1':
+        filteredData = filteredData.filter((item) => item.offer.guests == 1);
+        break;
+      case '2':
+        filteredData = filteredData.filter((item) => item.offer.guests == 2);
+        break;
+    }
+  }
+
+  return filteredData;
+};
+
+export { filterData };
--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
 
 'use strict';
 
-const adCounter = 10;
-
 import './data.js';
 import './scenarios.js';
 import './map.js';
@@ -13,21 +11,36 @@ import './server.js';
 import './filtration.js';
 import { generation } from './map.js';
 import { getData } from './server.js';
+import { filterData } from './filtration.js';
+
+const housingTypeSelect = document.querySelector('#housing-type');
+const housingPriceSelect = document.querySelector('#housing-price');
+const housingRoomSelect = document.querySelector('#housing-rooms');
+const housingGuestSelect = document.querySelector('#housing-guests');
+
+const filterChange = () => {
+  const selectedType = housingTypeSelect.value;
+  const selectedPrice = housingPriceSelect.value;
+  const selectedRoom = housingRoomSelect.value;
+  const selectedGuest = housingGuestSelect.value;
+  const filteredData = filterData(
+    data,
+    selectedType,
+    selectedPrice,
+    selectedRoom,
+    selectedGuest
+  );
+  generation(filteredData);
+};
+
+housingTypeSelect.addEventListener('change', filterChange);
+housingPriceSelect.addEventListener('change', filterChange);
+housingRoomSelect.addEventListener('change', filterChange);
+housingGuestSelect.addEventListener('change', filterChange);
 
 let data = [];
 
 getData((responseData) => {
   data = responseData;
   generation(data);
-});
-
-const housingTypeSelect = document.querySelector('#housing-type');
-
-const typeFilterChange = housingTypeSelect.addEventListener('change', () => {
-  const selectedType = housingTypeSelect.value;
-  let filteredData = data;
-  if (selectedType !== 'any') {
-    filteredData = data.filter((item) => item.offer.type === selectedType);
-  }
-  generation(filteredData);
 });
